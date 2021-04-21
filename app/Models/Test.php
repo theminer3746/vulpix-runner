1e58a123640b9479b2f24ed8a9cc98a94e41aded
theminer3746 <me@theminerdev.com>
2021-04-21 15:30:08 +0000

    Create Test model

---
+++ app/Models/Test.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Runner;
+
+class Test extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'apk_id',
+        'app_version',
+        'assigned_at',
+        'done_at',
+    ];
+
+    protected $casts = [
+        'assigned_at' => 'datetime',
+        'done_at' => 'datetime',
+    ];
+
+    public function runner()
+    {
+        return $this->belongsTo(Runner::class);
+    }
+}
