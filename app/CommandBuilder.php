029e719c36c3e31147748505840ec38b46a8cfc6
theminer3746 <me@theminerdev.com>
2021-05-12 14:10:41 +0000

    Add more arguments to CommandBuilder

--- app/CommandBuilder.php
+++ app/CommandBuilder.php
@@ -4,18 +4,79 @@ namespace App;
 
 class CommandBuilder
 {
+    protected string $proxyPort;
+    protected string $systemPort;
+    protected string $appiumPort;
+    protected string $endPoint;
+    
     public function __construct(
         protected string $deviceIp,
         protected string $devicePort,
         protected string $appId,
-        protected string $proxyIp,
-        protected string $proxyPort
+        protected string $proxyIp
     ) {
 
     }
 
+    public function setProxyPort(string $proxyPort)
+    {
+        $this->proxyPort = $proxyPort;
+
+        return $this;
+    }
+
+    public function setSystemPort(string $systemPort)
+    {
+        $this->systemPort = $systemPort;
+    
+        return $this;
+    }
+
+    public function setAppiumPort(string $appiumPort)
+    {
+        $this->appiumPort = $appiumPort;
+    
+        return $this;
+    }
+
+    public function setEndPoint(string $endPoint)
+    {
+        $this->endPoint = $endPoint;
+    
+        return $this;
+    }
+
     public function getCommand()
     {
-        return "python3 main.py $this->deviceIp:$this->devicePort $this->appId $this->proxyIp";
+        $command = [
+            'python3',
+            'main.py',
+            "$this->deviceIp:$this->devicePort",
+            $this->appId,
+            $this->proxyIp,
+        ];
+        // $command = "python3 main.py $this->deviceIp:$this->devicePort $this->appId $this->proxyIp";
+    
+        if (isset($this->proxyPort))
+        {
+            $command[] = "--proxy_port $this->proxyPort";
+        }
+
+        if (isset($this->systemPort))
+        {
+            $command[] = "--system_port $this->systemPort";
+        }
+
+        if (isset($this->appiumPort))
+        {
+            $command[] = "--appium_port $this->appiumPort";
+        }
+
+        if (isset($this->endPoint))
+        {
+            $command[] = "--endpoint $this->endPoint";
+        }
+
+        return implode(" ", $command);
     }
 }
