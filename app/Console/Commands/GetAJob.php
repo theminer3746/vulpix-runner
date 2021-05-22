5207013659b9a9109bbbda6d99ad139476db87f2
theminer3746 <me@theminerdev.com>
2021-05-22 22:01:12 +0000

    Add missing new line

--- app/Console/Commands/GetAJob.php
+++ app/Console/Commands/GetAJob.php
@@ -176,7 +176,7 @@ class GetAJob extends Command
                 '--endpoint', "https://vulpix-real-backend.theminerdev.com/api/results",
                 '--timeout', '600',
             ]);
-            echo("Running : $staticCommand");
+            echo("Running : $staticCommand\n");
             exec("cd $baseDir && cd flowdroid-automated && $staticCommand", $staticResult, $staticResultCode);
             var_dump($staticResult);
 
