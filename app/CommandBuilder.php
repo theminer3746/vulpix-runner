90082e1b1a7ab3585ab0bb78305157f72ccc3020
theminer3746 <me@theminerdev.com>
2021-05-28 03:01:25 +0000

    Add uuid Code formatting

--- app/CommandBuilder.php
+++ app/CommandBuilder.php
@@ -9,14 +9,14 @@ class CommandBuilder
     protected string $appiumPort;
     protected string $endPoint;
     protected string $timeout;
-    
+    protected string $uuid;
+
     public function __construct(
         protected string $deviceIp,
         protected string $devicePort,
         protected string $appId,
         protected string $proxyIp
     ) {
-
     }
 
     public function setProxyPort(string $proxyPort)
@@ -29,28 +29,35 @@ class CommandBuilder
     public function setSystemPort(string $systemPort)
     {
         $this->systemPort = $systemPort;
-    
+
         return $this;
     }
 
     public function setAppiumPort(string $appiumPort)
     {
         $this->appiumPort = $appiumPort;
-    
+
         return $this;
     }
 
     public function setEndPoint(string $endPoint)
     {
         $this->endPoint = $endPoint;
-    
+
         return $this;
     }
 
     public function setTimeout(string $timeout)
     {
         $this->timeout = $timeout;
-    
+
+        return $this;
+    }
+
+    public function setUuid(string $uuid)
+    {
+        $this->uuid = $uuid;
+
         return $this;
     }
 
@@ -64,30 +71,29 @@ class CommandBuilder
             $this->proxyIp,
         ];
         // $command = "python3 main.py $this->deviceIp:$this->devicePort $this->appId $this->proxyIp";
-    
-        if (isset($this->proxyPort))
-        {
+
+        if (isset($this->proxyPort)) {
             $command[] = "--proxy_port $this->proxyPort";
         }
 
-        if (isset($this->systemPort))
-        {
+        if (isset($this->systemPort)) {
             $command[] = "--system_port $this->systemPort";
         }
 
-        if (isset($this->appiumPort))
-        {
+        if (isset($this->appiumPort)) {
             $command[] = "--appium_port $this->appiumPort";
         }
 
-        if (isset($this->endPoint))
-        {
+        if (isset($this->endPoint)) {
             $command[] = "--endpoint $this->endPoint";
         }
 
-        if (isset($this->timeout))
-        {
+        if (isset($this->timeout)) {
             $command[] = "--timeout $this->timeout";
+        }
+
+        if (isset($this->uuid)) {
+            $command[] = "--uuid $this->uuid";
         }
 
         return implode(" ", $command);
